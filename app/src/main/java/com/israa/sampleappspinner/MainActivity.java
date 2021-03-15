93521e4f4ed47e3f0c98e9f2064b06624d56597b
israa <israa.naser2017@gmail.com>
2021-03-15 14:27:35 +0200

    ver 1.1

--- app/src/main/java/com/israa/sampleappspinner/MainActivity.java
+++ app/src/main/java/com/israa/sampleappspinner/MainActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         spinner = findViewById(R.id.spnLanguages);
+        populateSpinner();
+    }
+
+    private void populateSpinner() {
+        ArrayList<String> data =new ArrayList<>();
+        data.add("Java");
+        data.add("C#");
+        data.add("Php");
+        data.add("C++");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this ,android.R.layout.simple_spinner_item,data);
+        spinner .setAdapter(adapter);
     }
 
     public void btn_onClick(View view) {
